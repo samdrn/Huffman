71a24821e52ed6ddebc62d5bbbf326abc01c045c
samd <samuelmrn66@gmail.com>
2025-07-24 15:29:29 -0600

    Update NodoHuffman.cs

--- Huffman/NodoHuffman.cs
+++ Huffman/NodoHuffman.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace Huffman
 {
+    // Esta clase representa un Nodo en el árbol binario de Huffman
     public class NodoHuffman
     {
-        public char? Simbolo { get; set; }
-        public int Frecuencia { get; set; }
-        public NodoHuffman Izquierda { get; set; }
-        public NodoHuffman Derecha { get; set; }
+        public char? Simbolo { get; set; } // Carácter que representa (si es una hoja) 
+        public int Frecuencia { get; set; } // Frecuencia del carácter
+        public NodoHuffman Izquierda { get; set; } // Rama Izquierda
+        public NodoHuffman Derecha { get; set; } // Rama Derecha
 
-        public bool EsHoja => Izquierda == null && Derecha == null; 
+        public bool EsHoja => Izquierda == null && Derecha == null; // Verifica si el nodo es una hoja
     }
 }
